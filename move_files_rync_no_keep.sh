d18f52dddc301aec6d1e640dd802b6db61b115e9
Giannakopoulos John <giannakopoulosj@gmail.com>
2016-09-09 13:11:49 +0200

    Update move_files_rync_no_keep.sh

--- move_files_rync_no_keep.sh
+++ move_files_rync_no_keep.sh
@@ -7,5 +7,4 @@ _REMOTE_USER="remote_user"
 _REMOTE_HOST="foo.bar.lan"
 _REMOTE_PATH="foo/bar"
 
-rsync --remove-source-files -zvh ssh --include "$_INCLUDE_PATTERN" --exclude '*' backup.tar "$_REMOTE_USER"@"$_REMOTE_HOST":"$_REMOTE_PATH"
-
+rsync -zvh -e ssh --remove-source-files "$_TRANSFER_FILE" "$_REMOTE_USER"@"$_REMOTE_HOST":"$_REMOTE_PATH"
