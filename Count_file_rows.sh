b193f7d02ea4fbead9d2ad859f74506565e8a8d8
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-03-31 16:22:21 +0200

    Create Count_file_rows.sh

---
+++ Count_file_rows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Count rows of files.
+#-print0  print the full file name on the standard output, followed by a null character
+#xargs -0 Input  items  are  terminated by a null character instead of by whitespace
+
+
+find . -type f -name "*pattern*" -print0 | xargs -0 wc -l
