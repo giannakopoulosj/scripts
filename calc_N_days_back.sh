47408ec4ec2d516d5e6643c830f002a49dcddcde
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-04-03 17:48:29 +0200

    Update calc_N_days_back.sh

--- calc_N_days_back.sh
+++ calc_N_days_back.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#Working in Linux not in SunOS/AIX
-#to impelement check
 
 
+
+#Linux
 _DAYSBACK=$(date -d "-$1 day" +'%Y%m%d')
 echo $_DAYSBACK
+
+#non linux systems AIX??? / SOLARIS
+_DAYS=1
+echo $(TZ=a$(((24*$_DAYS))) date +'%Y%m%d')
