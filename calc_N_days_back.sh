eeefe89616717f0c5cfee348b70e403f6def190d
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-04-03 17:50:50 +0200

    Update calc_N_days_back.sh
    added solaris way and example

--- calc_N_days_back.sh
+++ calc_N_days_back.sh
@@ -9,3 +9,13 @@ echo $_DAYSBACK
 #non linux systems AIX??? / SOLARIS
 _DAYS=1
 echo $(TZ=a$(((24*$_DAYS))) date +'%Y%m%d')
+
+#TEST FOR SOLARIS
+_A_DAY=24
+_TODAY=$(date +%Y%m%d)
+_TWO_DAYS=$(TZ=CST+$(((2*$_A_DAY))) date +'%Y%m%d')
+_THREE_DAYS=$(TZ=CST+$(((3*$_A_DAY))) date +'%Y%m%d')
+
+echo "Today is $_TODAY"
+echo "Two days back was $_TWO_DAYS"
+echo "Three days back was $_THREE_DAYS"
