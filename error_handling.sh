cca4d93dcf2181874c03853de23d62f5832872e3
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-07-04 12:06:26 +0200

    Update error_handling.sh
    complete rework and option to ignore with flags

--- error_handling.sh
+++ error_handling.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 
-#FUNCTION_CODE_HERE
+#############################################FUNCTION_CODE_HERE
 
-function error {
+function error_banner ()
+{
 
     echo "----------------------------"
     echo "|   In case his job fails  |"
     echo "|     Please contact       |"
     echo "|          XXX XXX         |"
-    echo "----------------------------"               
+    echo "----------------------------"
 }
 
-function completed {
+function completed_banner ()
+{
 
     echo "----------------------------"
     echo "|      Execution was       |"
     echo "|  completed successfully  |"
-    echo "----------------------------"               
+    echo "----------------------------"
 }
 
-
-#MAIN_CODE_HERE
-
-function main {
-
-
-return $exit_code
+status_chk ()
+{
+case $1 in
+    0)
+    completed_banner
+    echo "script stopped at $(date)"
+    exit 0
+  ;;
+    1)
+    error_banner
+    echo "Exit code was 1..."
+    echo "script stopped at $(date)"
+    exit 1
+  ;;
+    8)
+    if [ $2='sftp' ] && [ $3='ignore_no_file' ]
+    then
+    completed_banner
+    exit 0
+    fi
+    error_banner
+    echo "script stopped at $(date)"
+    exit 1
+  ;;
+    *)
+    error_banner
+    echo "script stopped at $(date)"
+    exit 99
+  ;;
+esac
 }
 
-main
-error_validation=$?
-if [ $error_validation -ne 0 ]
-then
-error
-else 
-completed
-exit 0
-fi
+#####################################MAIN_CODE_HERE
+status_chk 8 sftp ignore_no_file
+
