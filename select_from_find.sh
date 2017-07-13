301cc3e27ba2326f8a1f88b46b6f0f4c1092de74
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-07-13 17:57:24 +0200

    Create select_from_find.sh

---
+++ select_from_find.sh
@@ -0,0 +1,22 @@
+When I use find, it often finds multiple results like
+
+find -name pom.xml
+./projectA/pom.xml
+./projectB/pom.xml
+./projectC/pom.xml
+I often want to select only a specific result, (e.g edit ./projectB/pom.xml). Is there a way to enumerate find output and select a file to pass into another application? like:
+
+find <print line nums?> -name pom.xml
+1 ./projectA/pom.xml
+2 ./projectB/pom.xml
+3 ./projectC/pom.xml
+
+!! | <get 2nd entry> | xargs myEditor
+?
+
+select file in $(find -name pom.xml); do
+  $EDITOR "$file"
+  break
+done
+
+--( IFS=$'\n'; select file in $(find -maxdepth 2 -name '*.txt'); do echo "$file"; done; )
