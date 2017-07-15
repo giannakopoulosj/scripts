d13d15170d8f0d4e9752299dcdb8d2557372c93c
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-07-15 16:29:35 +0300

    Update COMMAND_ssh_commands.sh

--- COMMAND_ssh_commands.sh
+++ COMMAND_ssh_commands.sh
@@ -3,5 +3,9 @@
 #SFTP on specific port
 sftp -oPort=1234 user@123.456.789.012
 
+sftp -oIdentityFile=other_rsa user@123.456.789.012
+
+sftp -i ther_rsa user@123.456.789.012
+
 #Change working directory direct on sftp connection
 sftp user@123.456.789.012:/home/something
