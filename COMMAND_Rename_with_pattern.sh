5601319b2dd4c9683a9e6b375e1bf2a18933aeae
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-07-15 16:33:20 +0300

    Update COMMAND_Rename_with_pattern.sh

--- COMMAND_Rename_with_pattern.sh
+++ COMMAND_Rename_with_pattern.sh
@@ -2,5 +2,6 @@
 
 #Rationalize & more examples:
 #http://tldp.org/LDP/abs/html/parameter-substitution.html
+#remove suffix
 
-for file in *.old; do mv "$file" "${file%%.old}";
+for file in *.old; do mv "$file" "${file%%.old}"; done
