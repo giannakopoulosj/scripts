b727cc1af39ff6363e3a54ab8581863d015f537d
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-07-19 11:33:18 +0200

    Update and rename awk_scripts.awk to file_data_manipulation.sh

--- awk_scripts.awk
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/awk
-
-#check column legth and print file name with separator ","
-awk -F',' '{ if(length($1) < 1) {print FILENAME}}' foo.bar.file
---
+++ file_data_manipulation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/awk
+
+#check column legth and print file name with separator ","
+awk -F',' '{ if(length($1) < 1) {print FILENAME}}' foo.bar.file
+
+#remove duplicate rows from file and create a new one.
+#-u = unique
+#-m = merges (used not to sort the file)
+#-f'|' = speparator is '|'
+#-k2,2 = 2nd column 2 duplicates
+sort -um -f'|' -k2,2 your.file
