41e2c0ba790b985d706d1de9dd1ec514c061702d
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-10-10 16:49:53 +0200

    Update Check_if_analyzed.sql

--- Check_if_analyzed.sql
+++ Check_if_analyzed.sql
@@ -10,25 +10,25 @@ Date on which this table was most recently analyzed
 
 
 --Tables
-select owner,table_name,last_analyzed, global_stats
+select owner,table_name,last_analyzed, global_stats,NUM_ROWS
 from dba_tables
 where owner not in ('SYS','SYSTEM')
 order by owner,table_name
 
 --Partitions
-select table_owner, table_name, partition_name, last_analyzed, global_stats
+select table_owner, table_name, partition_name, last_analyzed, global_stats,NUM_ROWS
 from dba_tab_partitions
 where table_owner  not in ('SYS','SYSTEM')
 order by table_owner,table_name, partition_name
 
 --Index
-select owner, index_name, last_analyzed, global_stats
+select owner, index_name, last_analyzed, global_stats,NUM_ROWS
 from dba_indexes
 where owner not in ('SYS','SYSTEM')
 order by owner, index_name
 
 --Partiion Index
-select index_owner, index_name, partition_name, last_analyzed, global_stats
+select index_owner, index_name, partition_name, last_analyzed, global_stats,NUM_ROWS
 from dba_ind_partitions
 where index_owner not in ('SYS','SYSTEM')
 order by index_owner, index_name, partition_name
