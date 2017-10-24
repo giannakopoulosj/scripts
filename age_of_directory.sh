560c3a713ebf5c182c413f2559e2f65068c89426
Giannakopoulos John <giannakopoulosj@gmail.com>
2017-10-24 12:11:09 +0200

    Create age_of_directory.sh

---
+++ age_of_directory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+time=$((($(date +"%s")-$(stat --printf="%Y" $1))))
+echo $time
+if [ $time -gt 3600 ]
+then
+echo "Time of last modification as seconds since Epoch is $time for Directory: $1"
+exit 1
+else
+echo "Time of last modification as seconds since Epoch is $time for Directory: $1"
+exit 0
+fi
