bc811188aeb0558555389674a79c15c2f39bcb70
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2018-08-23 09:30:53 +0300

    Create yes_no.sh

---
+++ yes_no.sh
@@ -0,0 +1,17 @@
+YesOrNo() {
+        while :
+        do
+                read -p 'Do you want to Continue (yes/no?): ' answer
+                case "${answer}" in
+                    [yY]|[yY][eE][sS]) exit 0 ;;
+                        [nN]|[nN][oO]) exit 1 ;;
+                esac
+        done
+}
+
+
+if $( YesOrNo ); then
+        echo "Ans is yes.. Do something....."
+else
+        echo "Ans is No... skip.."
+fi
