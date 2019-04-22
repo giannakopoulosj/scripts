c49bbfa1f00f035145aef6acc23953f93efd3b1d
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2019-04-22 13:38:15 +0300

    Update FINAL_calculate_first_last_day.sh

--- FINAL_calculate_first_last_day.sh
+++ FINAL_calculate_first_last_day.sh
@@ -19,3 +19,10 @@ LASTDY=$(cal $PRVMTH $PRVYR | egrep "28|29|30|31" |tail -1 |awk '{print $NF}')
  
 echo First Day: 01-$PRVMTH-$PRVYR
 echo Last Day: $LASTDY-$PRVMTH-$PRVYR
+
+#Calculate 1st & last day of each month for given years.
+#for year in {2019..2021};do
+#for month in {01..12};do 
+#echo "01 - $(cal $month $year | egrep "28|29|30|31" |tail -1 |awk '{print $NF}') $month $year"
+#done
+#done
