e2765f8ed0df58572d6b14d2e2cc0dd7670a3d2d
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2019-07-17 17:24:50 +0300

    Update NMON.sh

--- NMON.sh
+++ NMON.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#10 0 * * * NMON.sh
 /usr/bin/topas_nmon -^ -T -F /patches/nmon/nmon_$(date +%d%m%Y)_$(hostname).csv -s120 -c719
 chmod 666 /patches/nmon/nmon_$(date +%d%m%Y)_$(hostname).csv
