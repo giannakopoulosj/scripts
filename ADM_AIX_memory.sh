28e42c5b4463076546734f5ea0f384eefae3b24c
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2019-12-10 12:41:32 +0200

    Update ADM_AIX_memory.sh

--- ADM_AIX_memory.sh
+++ ADM_AIX_memory.sh
@@ -1,1 +1,2 @@
+#more info: https://www.ibm.com/support/knowledgecenter/en/ssw_aix_72/performance/mem_use_processes.html
 svmon -Pt15 | perl -e 'while(<>){print if($.==2||$&&&!$s++);$.=0 if(/^-+$/)}'
