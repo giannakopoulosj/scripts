0441218a71d8722537907548953d859549683a71
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-01-03 14:35:10 +0200

    Update ADM_ibm_hmc_scripts.sh

--- ADM_ibm_hmc_scripts.sh
+++ ADM_ibm_hmc_scripts.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+
 #Virtual FC
 for x in $(lssyscfg -r sys -F name); 
 do 
 lshwres -r virtualio -m $x --rsubtype fc --level lpar -F lpar_name,slot_num,wwpns --header |grep -v null;
 done
-
 #Physical FC
 for x in $(lssyscfg -r sys -F name); 
 do 
 lshwres -r io -m $x --rsubtype slotchildren -F lpar_name,wwpn | grep -v null;
 done
-
 #Network MAC
 for x in $(lssyscfg -r sys -F name);  do
 lshwres -r virtualio --rsubtype eth --level lpar -m $x -F lpar_name,mac_addr; 
