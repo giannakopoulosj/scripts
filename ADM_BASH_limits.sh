c73a33d15b385fd28bf5e84bbe0183764de2386a
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-01-28 14:11:24 +0200

    Update ADM_BASH_limits.sh

--- ADM_BASH_limits.sh
+++ ADM_BASH_limits.sh
@@ -2,3 +2,5 @@
 
 paste <(grep 'processes\| open files\|pending signals' /proc/self/limits | cut -c27-38)       <(i=`whoami` ; ps --no-headers -U $i -u $i u | wc -l ; lsof -u $i | tail -n +2 | awk {'print $9'} | wc -l; 
         ps -u $i -o pid= | xargs printf "/proc/%s/status\n" | xargs grep -s 'SigPnd' | sed 's/.*\t//' | paste -sd+ | bc ; ) |  while read a b ; do echo $a $b; done
+
+
