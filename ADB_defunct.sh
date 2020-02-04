56054fabc09b0227ebf68e4d28421b38fdc840f0
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-02-04 11:46:14 +0200

    Create ADB_defunct.sh

---
+++ ADB_defunct.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+defunct(){
+    echo "Children:"
+    ps -ef | head -n1
+    ps -ef | grep defunct
+    echo "------------------------------"
+    echo "Parents:"
+    ppids="$(ps -ef | grep defunct | awk '{ print $3 }')"
+    echo "$ppids" | while read ppid; do
+        ps -A | grep "$ppid"
+    done
+}
+
+defunct
