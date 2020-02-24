b0b06c7e3e4879072bd9e840b8128fa5c480fa14
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-02-24 09:24:18 +0200

    Update NMON.sh

--- NMON.sh
+++ NMON.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #0 * * * * NMON.sh
-/usr/bin/topas_nmon -^ -T -F /opt/NMON/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv -s15 -c240
-chmod 666 /opt/NMON/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv
-find /opt/NMON/ -type f -name "nmon*.csv" -mtime +10 -exec rm {} \;
+/fixes/NMON/nmon_stats.sh -^ -T -F /opt/NMON/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv -s15 -c240
+chmod 666 /fixes/NMON/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv
+find /fixes/NMON/ -type f -name "nmon*.csv" -mtime +10 -exec rm {} \;
 
