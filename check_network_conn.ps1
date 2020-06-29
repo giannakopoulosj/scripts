284d62bbbcf34c9358e0db305cfda7c7d64b24e6
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-06-29 17:28:33 +0300

    Create check_network_conn.ps1

---
+++ check_network_conn.ps1
@@ -0,0 +1,43 @@
+$portToChecks = '80','443'
+$myName = $env:computername
+
+ 
+
+GC .\IpInput.txt | %{
+
+ 
+
+foreach ($portToCheck in $portToChecks) {
+    $server_name = $_
+    If ( Test-Connection $_ -Quiet -Count 1 ) {
+    
+        try {       
+        $null = New-Object System.Net.Sockets.TCPClient -ArgumentList  $server_name, $portToCheck
+            
+               $props = @{
+                    Result=$server_name + ',' + $portToCheck + ',Yes,' + $myName
+               }
+        Write-Host "$server_name , $portToCheck set" -b Green
+        }
+
+ 
+
+        catch {
+                $props = @{
+                    Result=$server_name + ',' + $portToCheck + ',No,' + $myName
+                }    
+        Write-Host "$server_name , $portToCheck not set" -b Red
+        }
+    }
+    Else {
+        $props = @{
+                    Result=$server_name + ',' + $portToCheck + ',No ping,' + $probeName
+        }
+    Write-Host "$server_name $portToCheck No ping" -b Red
+    }
+
+ 
+
+    New-Object PsObject -Property $props
+} 
+}| Set-Content -Path ".\IpOutputResults.txt"
