f85d1ca53e95db4dd2a26887c18938310a33b603
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-10-09 14:12:20 +0300

    Update myhost_wrapper.sh

--- automate_etal/bulkcopysshid/myhost_wrapper.sh
+++ automate_etal/bulkcopysshid/myhost_wrapper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 while IFS=" " read -r server username password;do
-timeout 3 ssh -n $username@$server 'exit 66'
+timeout 3 ssh -o BatchMode=yes -n $username@$server 'exit 66' 
+#timout returns code 124 and BatchMode=yes if not with key 255
 if [ $? -eq 66 ] ;then
 echo "Key exist on $server"
 else
