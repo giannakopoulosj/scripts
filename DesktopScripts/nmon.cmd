e87b78ff6c19ac2de45ce313d96863d9194bf606
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2020-10-15 12:23:01 +0300

    Add files via upload

--- DesktopScripts/nmon.cmd
+++ DesktopScripts/nmon.cmd
@@ -1,3 +1,4 @@
 ECHO OFF
+IF NOT EXIST "C:\Users\%USERNAME%\Desktop\NMON\" md "C:\Users\%USERNAME%\Desktop\NMON\"
 del "C:\Users\%USERNAME%\Desktop\NMON\charts" /f /s /q
 java -jar C:\Users\%USERNAME%\Desktop\MyScripts\NMONVisualizer_2019-04-06.jar com.ibm.nmon.ReportGenerator C:\Users\%USERNAME%\Desktop\NMON
