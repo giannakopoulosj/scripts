673630ab458162061f27ef7843fe7314f405ec76
GiannakopoulosJ <giannakopoulosj@gmail.com>
2021-02-04 09:27:13 +0200

    Update NMON.sh

--- AdminScripts/NMON.sh
+++ AdminScripts/NMON.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #0 * * * * NMON.sh
-/usr/bin/topas_nmon -^ -T -F /opt/NMON/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv -s15 -c240
-chmod 666 /fixes/NMON/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv
-find /fixes/NMON/ -type f -name "nmon*.csv" -mtime +10 -exec rm {} \;
+nmon_file_path="/opt/NMON"
+/usr/bin/topas_nmon -^ -T -F $nmon_file_path/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv -s15 -c240
+chmod 666 $nmon_file_path/nmon_$(date +%d%m%Y_%H%m)_$(hostname).csv
+find $nmon_file_path/ -type f -name "nmon*.csv" -mtime +10 -exec rm {} \;
 
