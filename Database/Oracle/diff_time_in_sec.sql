c0a001cac9a2e5ffee0406dce5f616074c0daaa8
Giannakopoulos Ioannis <giannakopoulosj@gmail.com>
2021-02-20 17:59:43 +0200

    Restracture

--- Database/Oracle/diff_time_in_sec.sql
+++ Database/Oracle/diff_time_in_sec.sql
@@ -1,5 +1,5 @@
 select 
 trunc(mod(end_date - start_date,1)*24)*60*60 + -- Hours to seconds difference between Start_Date: DD/MM/YYYY HH24:MM:SS - End_Date: DD/MM/YYYY HH24:MM:SS
-trunc( mod(mod(end_date - start_date,1)*24,1)*60)*60 + -- Min to seconds between Start_Date: DD/MM/YYYY HH24:MM:SS - End_Date: DD/MM/YYYY HH24:MM:SS
+trunc(mod(mod(end_date - start_date,1)*24,1)*60)*60 + -- Min to seconds between Start_Date: DD/MM/YYYY HH24:MM:SS - End_Date: DD/MM/YYYY HH24:MM:SS
 trunc(mod(mod(mod(end_date - start_date,1)*24,1)*60,1)*60 ) as secs --Seconds between Start_Date: DD/MM/YYYY HH24:MM:SS - End_Date: DD/MM/YYYY HH24:MM:SS
 from dual;
--- automate_etal/Remove_suffix_wildcard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Rationalize & more examples:
-#http://tldp.org/LDP/abs/html/parameter-substitution.html
-#remove suffix
-
-for file in *.old; do mv "$file" "${file%%.old}"; done
